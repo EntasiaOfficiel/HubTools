38589d7fce24bcb6d0d41e0aecc90350c57459fc
iTrooz_ <team.mortelgames@gmail.com>
2020-06-04 18:32:44 +0200

    Petite surprise :)

--- src/fr/entasia/hubtools/listeners/ProtectionListeners.java
+++ src/fr/entasia/hubtools/listeners/ProtectionListeners.java
@@ -6,10 +6,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.util.Vector;
 
 import static fr.entasia.hubtools.Main.buildToggle;
 
@@ -33,8 +35,25 @@ public class ProtectionListeners implements Listener {
 	@EventHandler
 	public static void damage(EntityDamageEvent e) {
 		e.setCancelled(true);
-		if (!(e.getEntity() instanceof Player)) e.getEntity().remove();
-		else if (e.getCause() == EntityDamageEvent.DamageCause.VOID) Main.tpLobby((Player) e.getEntity());
+		if(e.getEntity() instanceof Player){
+			if (e.getCause() == EntityDamageEvent.DamageCause.VOID) Main.tpLobby((Player) e.getEntity());
+		}else e.getEntity().remove();
+	}
+
+	@EventHandler
+	public static void damage2(EntityDamageByEntityEvent e){
+		e.setCancelled(true);
+		if(e.getDamager() instanceof Player){
+			Vector v = e.getEntity().getVelocity();
+			v.add(e.getDamager().getLocation().getDirection().setY(0).divide(new Vector(5, 5, 5)));
+
+			if(v.getX()>4)v.setX(4);
+			else if(v.getX()<-4)v.setX(-4);
+
+			if(v.getZ()>4)v.setZ(4);
+			else if(v.getZ()<-4)v.setZ(-4);
+			e.getEntity().setVelocity(v);
+		}
 	}
 
 	@EventHandler
